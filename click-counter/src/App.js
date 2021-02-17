363fd760865ab90c9663bfe70502a3be9f39f4d1
Rafael Lopez <rafael.lopez@bbd.ca>
2021-02-16 23:03:46 -0500

    CLICK COUNTER: First data-test attribute test

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -3,21 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // - data-test attribute to test rendering
+    //    top level elemnt of component
+    // - using data-test we ensure our component is the one rendered, not just any other component rendered!
+    // - why new attribute? Why not id or class?
+    //    id and class have uses in production app
+    //    these might be changed in the future, which will breake your test
+    //    data-test is only for testing
+    //    conventional, but you can choose any name
+    // - if you don't want it in production, there's a way to get rid of these data-test attributes
+    <div data-test="component-app" className="App">
+
     </div>
   );
 }
