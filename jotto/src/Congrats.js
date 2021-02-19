50ebe8e783772ad65ad3c5e17b7bec1271f622b5
Rafael Lopez <rafael.lopez@bbd.ca>
2021-02-19 12:03:09 -0500

    JOTTO: Congrats component + Tests

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -3,10 +3,21 @@ import React from 'react';
 /**
  * Functional React component for congratulatory message
  * @function
+ * @param {object} props React props
  * @returns {JSX.Element} - Render component (or null if 'success' prop is passed)
  */
-export default () => {
-  return (
-    <div></div>
-  );
+export default (props) => {
+    if(props.success) {
+      return (
+        <div data-test="component-congrats">
+          <span data-test="congrats-message">
+            Congratulations! You guessed the word!
+          </span>
+        </div>
+      );
+    } else {
+      return (
+        <div data-test="component-congrats"></div>
+      );
+    }
 }
