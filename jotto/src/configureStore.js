e0e08288785c76293a57a9fc6b7c567e0d627164
Rafael Lopez <rafael.lopez@bbd.ca>
2021-02-25 19:08:15 -0500

    JOTTO: Installing Redux Thunk

--- jotto/src/configureStore.js
+++ jotto/src/configureStore.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 //If your import path is a folder and don't include a file name, it will automatically use the index.js file.
 //This is a commonJS convention that is followed by webpack in create-react-app projects.
 //In this case, we import the 'reducers' folder, which basically results in the index.js file in that folder
 import rootReducer from './reducers';
+import ReduxThunk from 'redux-thunk';
 
-export default createStore(rootReducer);
+export const middlewares = [ReduxThunk];
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+
+export default createStoreWithMiddleware(rootReducer);
