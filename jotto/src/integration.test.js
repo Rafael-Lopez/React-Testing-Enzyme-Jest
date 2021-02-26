56d67f385338c4a95546d064feb622af035b0a03
Rafael Lopez <rafael.lopez@bbd.ca>
2021-02-25 19:50:12 -0500

    JOTTO: First Thunk test

--- jotto/src/integration.test.js
+++ jotto/src/integration.test.js
@@ -2,10 +2,32 @@ import { storeFactory } from '../test/testUtils';
 import { guessWord } from './actions';
 
 describe('guessWord action dispatcher', () => {
+  const secretWord = 'party';
+  const unsuccessfulGuess = 'train';
 
   describe('no guessed words', () => {
-    test('updates state correctly for unsuccessful guess', () => {
+    let store;
+    const initialState = { secretWord };
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
 
+    test('updates state correctly for unsuccessful guess', () => {
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [{
+          guessedWord: unsuccessfulGuess,
+          letterMatchCount: 3
+        }]
+      };
+
+      // dispatch and getState are methods available in the Store object
+      // https://redux.js.org/api/store
+      store.dispatch( guessWord(unsuccessfulGuess) );
+      const newState = store.getState();
+
+      expect(newState).toEqual(expectedState);
     });
     test('updates state correctly for successful guess', () => {
 
