2bf359566dd6dc8ad590c70efc5e859cb20c9080
Rafael Lopez <rafael.lopez@bbd.ca>
2021-02-28 13:50:37 -0500

    JOTTO: Add tests and code to clear Input box on submit

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -35,6 +35,7 @@ export class UnconnectedInput extends Component {
 
     if(guessedWord && guessedWord.length > 0) {
       this.props.guessWord(guessedWord);
+      this.setState({ currentGuess: '' });
     }
   }
 
