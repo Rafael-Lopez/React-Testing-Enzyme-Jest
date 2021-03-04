06aed0fbb77cb7f4d63485b617773589f6659c1e
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-03 21:44:33 -0500

    JOTTO (Hooks): Prepare Input for writing setState tests

--- jotto-hooks/src/Input.js
+++ jotto-hooks/src/Input.js
@@ -2,8 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ( {secretWord} ) => {
+  //Use hooks this way in order to be able to mock them.
+  //Do NOT use destructuring in import. E.g. import {useState} from 'react'
+  const [ currentGuess, setCurrentGuess ] = React.useState('');
+
   return (
-    <div data-test='input-component' />
+    <div data-test='input-component'>
+      <form className='form-inline'>
+        <input
+          data-test='input-box'
+          type='text'
+          className='mb-2 mx-sm-3'
+          placeholder='enter guess'
+        >
+        <button
+          data-test='submit-button'
+          className='btn btn-primary mb-2'>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
