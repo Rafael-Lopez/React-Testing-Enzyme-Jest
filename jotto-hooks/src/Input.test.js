23acfa765b27054e2200b435a491b445cc452bc0
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-03 21:05:51 -0500

    JOTTO (Hooks): Basic Input component

---
+++ jotto-hooks/src/Input.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr } from '../test/testUtils';
+import Input from './Input';
+
+/**
+ * Setup function for Input component
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<Input />);
+};
+
+test('renders Input component', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'input-component');
+
+  expect(component.length).toBe(1);
+});
