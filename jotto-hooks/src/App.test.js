49b50f5d59f7df6aa05fb3a69e9ebb567e931684
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-03 23:52:28 -0500

    JOTTO (Hooks): Test getSecretWord is called on App mount (useEffect)

--- jotto-hooks/src/App.test.js
+++ jotto-hooks/src/App.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import { findByTestAttr } from '../test/testUtils';
+import hookActions from './actions/hookActions';
 import App from './App';
+
+//We don't need this mock to do anything or return anything, we just need it to spy
+//on it to see if it's called in the appropriate place, and not run when we don't want it to
+const mockGetSecretWord = jest.fn();
 
 /**
  * Setup function for app component
- * @returns {ShallowWrapper}
+ * @returns {ReactWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  //to make sure the calls from past tests are not carried over to the next test
+  mockGetSecretWord.mockClear();
+  //we follow the same pattern to replace a real function with a mock
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  //To test useEffect we have to use mount() for now. Enzyme currently has a bug where useEffect
+  //is not called when using shallow(), only mount().
+  //https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 }
 
 test('App renders without error', () => {
@@ -17,4 +30,13 @@ test('App renders without error', () => {
   const component = findByTestAttr(wrapper, 'component-app');
 
   expect(component.length).toBe(1);
+});
+
+describe('`getSecretWord` calls', () => {
+  test('`getSecretWord` gets called on App mount', () => {
+    setup();
+
+    //check to see if secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
