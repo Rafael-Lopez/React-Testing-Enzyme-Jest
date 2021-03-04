ea8b01e6adc3c36b374c0d28519513804fdf9a0a
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-03 21:27:57 -0500

    JOTTO (Hooks): Test Input props

--- jotto-hooks/src/Input.test.js
+++ jotto-hooks/src/Input.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
 /**
  * Setup function for Input component
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord}/>);
 };
 
 test('renders Input component', () => {
@@ -17,4 +17,10 @@ test('renders Input component', () => {
   const component = findByTestAttr(wrapper, 'input-component');
 
   expect(component.length).toBe(1);
+});
+
+test('does not thrpw warning with expected props', () => {
+  const wrapper = setup();
+
+  checkProps(Input, { secretWord: 'party' });
 });
