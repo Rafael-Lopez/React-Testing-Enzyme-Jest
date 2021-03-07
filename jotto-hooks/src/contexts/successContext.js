68181ddf180ebb33f7c53d6ab701ac59cd0dae1a
Rafael Lopez <rafael.lopez@bbd.ca>
2021-03-07 12:13:07 -0500

    JOTTO (Hooks): Code success context (Context with State Embedded)

---
+++ jotto-hooks/src/contexts/successContext.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+//Context with state embedded
+//https://kentcdodds.com/blog/application-state-management-with-react
+const successContext = React.createContext();
+
+
+/**
+* @function useSuccess
+* @returns {array} successContext value, which is a state of [value, setter].
+*
+*/
+function useSuccess() {
+  const context = React.useContext(successContext);
+
+  if(!context) {
+    throw new Error('useSuccess must be used within a SuccessProvider');
+  }
+
+  return context;
+}
+
+/**
+* @function SuccessProvider
+* @param {object} props - props to pass through from declared component
+* @returns {JSX.Element} Provider component
+*/
+function SuccessProvider(props) {
+    const [success, setSuccess] = React.useState(false);
+
+    const value = React.useMemo(() => [success, setSuccess], [success]);
+
+    return <successContext.Provider value={value} {...props} />
+}
+
+export default { SuccessProvider, useSuccess }
